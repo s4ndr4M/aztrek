3eab2a761d3315487337f30fd087c8da26fdf8de
s4ndr4M <sandra.mesquita.pro@gmail.com>
2018-05-30 17:52:08 +0200

    Mise à jour du projet/Modification partie admin

--- admin/layout/footer.php
+++ admin/layout/footer.php
@@ -7,6 +7,7 @@
         <script src="<?php echo ADMIN_URL; ?>node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
         <script src="<?php echo ADMIN_URL; ?>node_modules/datatables/media/js/jquery.dataTables.min.js"></script>
         <script src="<?php echo ADMIN_URL; ?>node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js"></script>
+        <script src="<?php echo ADMIN_URL; ?>node_modules/select2/dist/js/select2.min.js"></script>
         <script src="<?php echo ADMIN_URL; ?>js/admin.js"></script>
       
     </body>
--- lib/functions.php
+++ lib/functions.php
@@ -1,28 +1,45 @@
 <?php
+if (!isset($_SESSION)) {
+    session_start();
+}
 
 /**
-*Debugger une variable
-*@para mixed $var la variable à afficher
-*@para
-**/
-
-
-function debug($var, bool $die=true) {
+ * Debugger une variable
+ * @param mixed $var La variable à afficher
+ * @param bool $die Arrêter l'execution
+ */
+function debug($var, bool $die = true) {
     echo "<pre>";
     print_r($var);
     echo "</pre>";
     if ($die) {
         die;
-        
     }
+}
+
+function currentUser() {
+    if (isset($_SESSION["id"])) {
+        return getOneUser($_SESSION["id"]);
+    }
+    return null;
+}
+
+/**
+ * Récupérer le chemin actuel (sans le nom du fichier)
+ * @return string Path
+ */
+function currentPath() {
+    return "http://" . $_SERVER["HTTP_HOST"] . substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/') + 1);
 }
 
 function getHeader(string $title) {
     require_once 'layout/header.php';
 }
+
 function getFooter() {
     require_once 'layout/footer.php';
 }
+
 function getMenu() {
     require_once 'layout/menu.php';
 }
