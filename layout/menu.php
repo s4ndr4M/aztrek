57cd6c4cc9a1b49b553596b3938ade31c68062db
s4ndr4M <sandra.mesquita.pro@gmail.com>
2018-06-01 13:45:54 +0200

    modification menu dans front

--- layout/menu.php
+++ layout/menu.php
@@ -1,7 +1,18 @@
+<?php
+require_once __DIR__ . '/../lib/functions.php';
+
+$user = currentUser();
+?>
+
 <nav>
     <ul>
         <li><a href="#">Accueil</a></li>
-        <li><a href="#">Pays</a></li>
+        <li><a href="#">Nos Destinations</a></li>
         <li><a href="#">Contact</a></li>
+        <?php if (!isset($utilisateur["id"])) : ?>
+            <li><a href="#">Login</a></li>
+        <?php else: ?>
+            <li><a href="#">Mon Compte</a></li>
+        <?php endif; ?>
     </ul>
 </nav>
